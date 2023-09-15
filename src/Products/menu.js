20b991e589f0ab339fe75062ec8410b8ef34cc81
Jonathan Santos <jonathan01347@gmail.com>
2023-09-15 14:16:27 -0300

    feat(app)Initial setup

---
+++ src/Products/menu.js
@@ -0,0 +1,215 @@
+const menulist = [
+  {
+    name: 'Yakisoba Tradicional (Grande)',
+    price: '59,50',
+    category: 'Yakisoba',
+  },
+  {
+    name: 'Yakisoba Tradicional (Médio)',
+    price: '48,50',
+    category: 'Yakisoba',
+  },
+  {
+    name: 'Frango Xadres (Grande)',
+    price: '50,00',
+    category: 'Frango',
+  },
+  {
+    name: 'Frango Xadres (Médio)',
+    price: '42,00',
+    category: 'Frango',
+  },
+  {
+    name: 'Yakisoba Vegetariano (Grande)',
+    price: '48,00',
+    category: 'Yaquisoba',
+  },
+  {
+    name: 'Yakisoba Vegetariano (Médio)',
+    price: '42,50',
+    category: 'Yaquisoba',
+  },
+  {
+    name: 'Yakisoba Camarão (Grande)',
+    price: '75,50',
+    category: 'Yaquisoba',
+  },
+  {
+    name: 'Yakisoba Camarão (Médio)',
+    price: '63,00',
+    category: 'Yaquisoba',
+  },
+  {
+    name: 'Carne com brócolis (Grande)',
+    price: '73,00',
+    category: 'Carne',
+  },
+  {
+    name: 'Carne com brócolis (Médio)',
+    price: '60,00',
+    category: 'Carne',
+  },
+  {
+    name: 'Costelinha (Grande)',
+    price: '48,00',
+    category: 'Carne',
+  },
+  {
+    name: 'Costelinha (Médio)',
+    price: '42,00',
+    category: 'Carne',
+  },
+  {
+    name: 'Filé acebolado (Grande)',
+    price: '63,00',
+    category: 'Carne',
+  },
+  {
+    name: 'Filé acebolado (Médio)',
+    price: '53,00',
+    category: 'Carne',
+  },
+  {
+    name: 'Frango a passarinho (Grande)',
+    price: '43,00',
+    category: 'frango',
+  },
+  {
+    name: 'Frango a passarinho (Médio)',
+    price: '38,00',
+    category: 'Frango',
+  },
+  {
+    name: 'Frango empanado (Grande)',
+    price: '40,00',
+    category: 'Frango',
+  },
+  {
+    name: 'Frango empanado (Médio)',
+    price: '35,00',
+    category: 'Frango',
+  },
+  {
+    name: 'Tilápia ao molho gengibre (Médio)',
+    price: '65,00',
+    category: 'Peixe',
+  },
+  {
+    name: 'Tilápia ao molho gengibre (Médio)',
+    price: '55,00',
+    category: 'Peixe',
+  },
+  {
+    name: 'Frango agridoce (Grande)',
+    price: '45,00',
+    category: 'Agridoce',
+  },
+  {
+    name: 'Frango agridoce (Médio)',
+    price: '38,00',
+    category: 'Agridoce',
+  },
+  {
+    name: 'Lombo agridoce (Grande)',
+    price: '50,00',
+    category: 'Agridoce',
+  },
+  {
+    name: 'Lombo agridoce (Médio)',
+    price: '40,00',
+    category: 'Agridoce',
+  },
+  {
+    name: 'Arroz Chop Suey (Grande)',
+    price: '34,00',
+    category: 'Arroz',
+  },
+  {
+    name: 'Arroz Chop Suey (Médio)',
+    price: '29,00',
+    category: 'Arroz',
+  },
+  {
+    name: 'Arroz Chop Suey com Camarão (Grande)',
+    price: '60,00',
+    category: 'Arroz',
+  },
+  {
+    name: 'Arroz Chop Suey com Camarão (Médio)',
+    price: '46,00',
+    category: 'Arroz',
+  },
+  {
+    name: 'Rolinho de Camarão com Alho Poró (2un)',
+    price: '17,00',
+    category: 'Rolinho',
+  },
+  {
+    name: 'Rolinho de Alho Poró (2un)',
+    price: '12,00',
+    category: 'Rolinho',
+  },
+  {
+    name: 'Rolinho Tradicional (2un)',
+    price: '7,50',
+    category: 'Rolinho',
+  },
+  {
+    name: 'Rolinho Romeu e Julieta',
+    price: '10,00',
+    category: 'Rolinho',
+  },
+  {
+    name: 'Rolinho napolitano',
+    price: '9,00',
+    category: 'Rolinho',
+  },
+  {
+    name: 'Refrigerante lata coca-cola',
+    price: '5,00',
+    category: 'Refrigerante',
+  },
+  {
+    name: 'Refrigerante lata fanta',
+    price: '5,00',
+    category: 'Refrigerante',
+  },
+  {
+    name: 'Refrigerante lata guaraná',
+    price: '5,00',
+    category: 'Refrigerante',
+  },
+  {
+    name: 'Refrigerante lata sprite',
+    price: '5,00',
+    category: 'Refrigerante',
+  },
+  {
+    name: 'Amendoim',
+    price: '1,50',
+    category: 'Molhos',
+  },
+  {
+    name: 'Molho agridoce',
+    price: '1,50',
+    category: 'Molhos',
+  },  
+  {
+    name: 'Molho shoyo',
+    price: '1,50',
+    category: 'Molhos',
+  },  
+  {
+    name: 'Molho shoyo com gengibre',
+    price: '1,50',
+    category: 'Molhos',
+  },
+  {
+    name: 'Alho frito',
+    price: '1,50',
+    category: 'Molhos',
+  },
+
+];
+
+module.exports = menulist;
