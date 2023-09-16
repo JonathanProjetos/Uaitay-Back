235f0d0a4657102c70139a39f5c9f1bbaa8f8fc7
Jonathan Santos <jonathan01347@gmail.com>
2023-09-15 22:37:38 -0300

    fix(config)change config

--- src/config/connection.js
+++ src/config/connection.js
@@ -3,11 +3,6 @@ const mongoose = require('mongoose');
 require = ('dotenv/config');
 
 const MONGO_DB_URL = process.env.MONGO_URL;
-/*
-12 1.152 (node:20) [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
-12 1.152 (Use `node --trace-deprecation ...` to show where the warning was created)
-*/
-mongoose.set('strictQuery', false);
 
 const connectToDataBase = (
   mongoDataBaseURI = MONGO_DB_URL,
