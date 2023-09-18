c4fb693ae5aa636f146eab409fbb9beb9306e262
Jonathan Santos <jonathan01347@gmail.com>
2023-09-18 20:10:57 -0300

    feat(app)create middleware error.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+require('express-async-errors')
 const cors = require('cors');
 const RouteMenu = require('./routes/RouteMenu');
+const RouteUser = require('./routes/RouteUser');
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 app.use('/', RouteMenu);
+app.use('/', RouteUser);
 
+app.use((err, _req, res, _next) => {
+  if (err.message.split('').includes('|')) {
+    const [status, message] = err.message.split('|');
+    res.status(Number(status)).json({ error: message });
+  } else {
+    console.error('Erro não Mapeado:', err.message);
+    res.status(500).json({ error: err.message });
+  }
+  
+});
 
 module.exports = app;
 
