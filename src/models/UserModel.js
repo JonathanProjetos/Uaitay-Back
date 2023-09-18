e6ff2d85568205dcac7cbac05d26b225ffe40087
Jonathan Santos <jonathan01347@gmail.com>
2023-09-18 20:16:05 -0300

    feat(UserModel)create schema  for user.

---
+++ src/models/UserModel.js
@@ -0,0 +1,14 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+},
+{
+  versionKey: false,
+}
+);
+
+const userModel = model('user', userSchema);
+
+module.exports = userModel;
