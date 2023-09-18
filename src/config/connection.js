c8a8a95a11b5319026fe524bac8c1b3de3321f1f
Jonathan Santos <jonathan01347@gmail.com>
2023-09-18 20:11:54 -0300

    feat(connection)adding database name.

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
-
-require = ('dotenv/config');
+require('dotenv/config');
 
 const MONGO_DB_URL = process.env.MONGO_URL;
 
-const connectToDataBase = (
-  mongoDataBaseURI = MONGO_DB_URL,
-) => mongoose.connect(mongoDataBaseURI);
+const connectToDataBase = async (mongoDataBaseName = 'uai-tay') => {
+  try {
+    const mongoDataBaseURI = `${MONGO_DB_URL}/${mongoDataBaseName}`;
+      mongoose.connect(mongoDataBaseURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Conectado ao banco de dados: ${mongoDataBaseName}`);
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
+};
 
 module.exports = connectToDataBase;
