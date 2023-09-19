5c07bcb6fa69d7eafba89dda98c917da19906819
Jonathan Santos <jonathan01347@gmail.com>
2023-09-19 20:25:50 -0300

    fix(userController)ange in the way information is passed on

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,7 @@ const userServices = require('../services/userServices');
 const userController = {
 
   getUser: async (req, res) => {
-    const { email, password } = req.body;
-    const user = await userServices.getUser(email, password);
+    const user = await userServices.getUser(req.body);
     res.status(200).json(user);
   },
 
