be12ab78890300947f87969b743ae952fcf4bb47
Jonathan Santos <jonathan01347@gmail.com>
2023-09-19 20:26:28 -0300

    feat(RouterMenu)create route for create-product

--- src/routes/RouteMenu.js
+++ src/routes/RouteMenu.js
@@ -4,5 +4,6 @@ const menuController = require('../controllers/menuController');
 const RouteMenu = Router();
 
 RouteMenu.get('/menu', menuController.getAllProductsMenu);
+RouteMenu.post('/create-product', menuController.createProductMenu);
 
 module.exports = RouteMenu;
