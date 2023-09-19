77af2e21d9b42f4d02a20f2a234980b73ced8e85
Jonathan Santos <jonathan01347@gmail.com>
2023-09-19 20:28:45 -0300

    fix(userServices)added validator for input data

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,8 +1,11 @@
 const products = require('../models/UserModel');
+const joiUser = require('../middlewares/joiUser');
 
 const userServices = {
 
-  getUser: async (email, password) => {
+  getUser: async (body) => {
+    const { email, password } = joiUser(body);
+
     const user = await products.findOne({ email, password });
 
     if (user === null) throw new Error('401|Usuario não encontrado');
