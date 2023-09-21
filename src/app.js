96a2e7dce1ae3049f84e795cf5bbe931ab990bef
Jonathan Santos <jonathan01347@gmail.com>
2023-09-20 23:57:29 -0300

    fix(app)changed settings cors

--- src/app.js
+++ src/app.js
@@ -12,9 +12,17 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: 'https://uai-tay-front.vercel.app/',
+  origin: 'https://uai-tay-front.vercel.app',
   credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://uai-tay-front.vercel.app');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(cookieParser());
 
