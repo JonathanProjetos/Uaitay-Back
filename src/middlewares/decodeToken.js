a913e0408d592c6c5b762a0c3f7b2279f3046a16
Jonathan Santos <jonathan01347@gmail.com>
2023-09-20 22:00:30 -0300

    feat(token)Added middleware token are decodeToken.

---
+++ src/middlewares/decodeToken.js
@@ -0,0 +1,15 @@
+const tokenFunction = require('./token');
+
+module.exports = {
+  // Middleware que verifica se o token é válido.
+  verifyToken: (req, _res, next) => {
+    try {
+      let token = req.cookies['token'];
+      const dados = tokenFunction.validateToken(token);
+      req.email = dados.email;
+      next();
+    } catch (err) {
+      throw new Error('404|Token not found');
+    }
+  },
+};
