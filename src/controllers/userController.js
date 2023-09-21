070a1476a8b32bb6ee69c4d474b965dabbd5b60b
Jonathan Santos <jonathan01347@gmail.com>
2023-09-20 22:02:08 -0300

    feat(userserController)Added endpoint for verify token.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,18 @@ const userServices = require('../services/userServices');
 const userController = {
 
   getUser: async (req, res) => {
-    const user = await userServices.getUser(req.body);
-    res.status(200).json(user);
+    const { menssage, token } = await userServices.getUser(req.body);
+
+    console.log(token);
+    res.cookie('token', token, { httpOnly: true, secure: false, path: '/', maxAge: 3600000 });
+    res.status(200).json({ menssage });
+  },
+  
+  checkAcessLogin: async (req, res) => {
+    const { email } = req;
+    console.log(email);
+    const message = await userServices.checkAcessLogin(email);
+    res.status(200).json(message);
   },
 
 }
