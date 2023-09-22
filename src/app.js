5e57fe2a62c6322c857891399a7ead2d3150c6f7
Jonathan Santos <jonathan01347@gmail.com>
2023-09-22 18:06:42 -0300

    fix(app)test end-point

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://uai-tay-front.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'https://uai-tay-front.vercel.app');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
