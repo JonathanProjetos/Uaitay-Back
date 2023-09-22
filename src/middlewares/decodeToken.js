6bd413eed97b57d8cbe1754194eed1c062caf9c0
Jonathan Santos <jonathan01347@gmail.com>
2023-09-22 18:14:25 -0300

    chore(decodedToken)added console log

--- src/middlewares/decodeToken.js
+++ src/middlewares/decodeToken.js
@@ -5,6 +5,7 @@ module.exports = {
   verifyToken: (req, _res, next) => {
     try {
       let token = req.cookies['token'];
+      console.log(token);
       const dados = tokenFunction.validateToken(token);
       req.email = dados.email;
       next();
