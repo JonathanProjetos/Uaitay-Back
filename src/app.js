d0d09c01409680be49ce548618d2b0a113d7aa0b
Jonathan Santos <jonathan01347@gmail.com>
2023-09-22 18:00:40 -0300

    fix(app)test end-point

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: 'https://uai-tay-front.vercel.app',
+  origin: 'https://uai-tay-front-jonathanprojetos.vercel.app/',
   credentials: true,
 }));
 
 app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://uai-tay-front.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://uai-tay-front-jonathanprojetos.vercel.app/');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
