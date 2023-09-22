f4efa90c3cace1a971030ee7b367eaedc5579333
Jonathan Santos <jonathan01347@gmail.com>
2023-09-22 18:34:59 -0300

    fix(userController)Added token in response json

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,22 @@
-const userServices = require('../services/userServices');
+  const userServices = require('../services/userServices');
 
-const userController = {
+  const userController = {
 
-  getUser: async (req, res) => {
-    const { menssage, token } = await userServices.getUser(req.body);
+    getUser: async (req, res) => {
+      const { menssage, token } = await userServices.getUser(req.body);
 
-    console.log(token);
-    res.cookie('token', token, { httpOnly: true, secure: true, path: '/', maxAge: 3600000 });
-    res.status(200).json({ menssage });
-  },
-  
-  checkAcessLogin: async (req, res) => {
-    const { email } = req;
-    console.log(email);
-    const message = await userServices.checkAcessLogin(email);
-    res.status(200).json(message);
-  },
+      console.log(token);
+      res.cookie('token', token, { httpOnly: true, secure: true, path: '/', maxAge: 3600000 });
+      res.status(200).json({ menssage, token });
+    },
+    
+    checkAcessLogin: async (req, res) => {
+      const { email } = req;
+      console.log(email);
+      const message = await userServices.checkAcessLogin(email);
+      res.status(200).json(message);
+    },
 
-}
+  }
 
-module.exports = userController;
+  module.exports = userController;
