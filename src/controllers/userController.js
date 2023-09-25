49ba94b3dab7dcf21523c4b4dbdf1b123658673b
Jonathan Santos <jonathan01347@gmail.com>
2023-09-25 20:35:56 -0300

    fix(userController)clean code and added sameSite in res.cookie

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,15 +4,12 @@
 
     getUser: async (req, res) => {
       const { menssage, token } = await userServices.getUser(req.body);
-
-      console.log("aquuuiiiiiii", token);
-      res.cookie('token', token, { httpOnly: true, secure: true, path: '/', maxAge: 3600000 });
+      res.cookie('token', token, { httpOnly: true, secure: true, path: '/', maxAge: 3600000, sameSite: 'None' })
       res.status(200).json({ menssage, token });
     },
     
     checkAcessLogin: async (req, res) => {
       const { email } = req;
-      console.log("Não contém email", email);
       const message = await userServices.checkAcessLogin(email);
       res.status(200).json(message);
     },
