6efe4d1e2fb67c1f841d0bc3220510140627dd4b
Jonathan Santos <jonathan01347@gmail.com>
2023-09-28 20:49:06 -0300

    feat(menuServices)Added algorithm to delete a product.

--- src/services/menuServices.js
+++ src/services/menuServices.js
@@ -9,7 +9,7 @@ const menuProductsServices = {
   },
 
   createProductMenu: async(data) => {
-    const { name, price, category } = joiProduct(data);
+    const { name, price, category } = joiProduct.validadeBodyCreateProduct(data);
 
     const allProducts = await products.find();
 
@@ -20,6 +20,21 @@ const menuProductsServices = {
     const newProduct = await products.create({ name, price, category});
     
     return newProduct;
+  },
+
+  deleteProduct: async (name) => {
+    const strigName = joiProduct.validadeNameFromDeleteProduct(name);
+
+    const allProducts = await products.find();
+
+    const productExists = allProducts.find((product) => product.name.toLowerCase() === strigName.toLowerCase());
+
+    if (!productExists) throw new Error('404|Produto não encontrado');
+
+    const  result  = await products.deleteOne({ name: productExists.name });
+
+    return result;
+
   }
 
 }
