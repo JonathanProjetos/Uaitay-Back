eee1d5d3766bc4128655ba7c33d4c7ca4157e7a2
Jonathan Santos <jonathan01347@gmail.com>
2023-10-13 14:41:09 -0300

    feat(joi)Create new body validade from updateproduct.

--- src/middlewares/joiProducts.js
+++ src/middlewares/joiProducts.js
@@ -27,7 +27,7 @@ const validadeBodyCreateProduct = (body) => {
   return value;
 }
 
-const validadeNameFromDeleteProduct = (name) => {
+const validadeNameProduct = (name) => {
   const schemaName = joi.string().required().messages({
     'string.empty': '400|O campo "name" não pode ser vazio',
     'any.required': '400|O campo "name" é obrigatório',
@@ -41,7 +41,34 @@ const validadeNameFromDeleteProduct = (name) => {
   return value;
 }
 
+
+const validadeBodyUpdateProduct = (body) => {
+  const schemaProduct = joi.object({
+    name: joi.string().min(5).required().messages({
+      'string.empty': '400|O campo "name" não pode ser vazio',
+      'any.required': '400|O campo "name" é obrigatório',
+      'string.min': '400|O campo "name" deve ter pelo menos 5 caracteres',
+    }),
+    price: joi.number().greater(0).required().messages({
+      'number.base': '400|O campo "price" deve ser um número',
+      'number.empty': '400|O campo "price" não pode ser vazio',
+      'any.required': '400|O campo "price" é obrigatório',
+      'number.greater': '400|O campo "price" deve ser maior que zero',
+    }).strict(),
+  })
+
+  const { error , value } = schemaProduct.validate(body);
+
+  if(error) {
+    throw error;
+  }
+  return value;
+}
+
+
+
 module.exports = {
   validadeBodyCreateProduct,
-  validadeNameFromDeleteProduct
+  validadeNameProduct,
+  validadeBodyUpdateProduct
 };
