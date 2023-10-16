73fd6164de251f8f9e373abb2f1429c4b3f88bc4
Jonathan Santos <jonathan01347@gmail.com>
2023-10-16 19:42:27 -0300

    fix(connection)adding database name in options

--- src/config/connection.js
+++ src/config/connection.js
@@ -3,13 +3,14 @@ require('dotenv/config');
 
 const MONGO_DB_URL = process.env.MONGO_URL;
 
-const connectToDataBase = async (mongoDataBaseName = 'uai-tay') => {
+const connectToDataBase = async (mongoDataBaseName = 'UaiTay') => {
   try {
-    const mongoDataBaseURI = `${MONGO_DB_URL}/${mongoDataBaseName}`;
-    await mongoose.connect(mongoDataBaseURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const mongoDataBaseURI = `${MONGO_DB_URL}`;
+      mongoose.connect(mongoDataBaseURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: mongoDataBaseName,
+      });
     console.log(`Conectado ao banco de dados: ${mongoDataBaseName}`);
   } catch (error) {
     console.error('Erro ao conectar ao banco de dados:', error);
