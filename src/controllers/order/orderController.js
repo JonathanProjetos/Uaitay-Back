952482db71b1bc14c3722beb6932a1ae2b79a54f
Jonathan Santos <jonathan01347@gmail.com>
2023-10-24 19:14:37 -0300

    feat(orderServices)added algoritm for request orders in controller layer.

--- src/controllers/order/orderController.js
+++ src/controllers/order/orderController.js
@@ -2,6 +2,11 @@ const orderServices = require('../../services/order/orderService');
 
 const  orderController = {
 
+  getAllOrders: async (_req, res) => {
+    const orders = await orderServices.getAllOrders();
+    res.status(200).json(orders);
+  },
+
   createOrder: async (req, res) => {
     const { body } = req;
     const result = await orderServices.createOrder(body);
