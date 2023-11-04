f038874f437df6ec148b578d646eacce4943a076
Jonathan Santos <jonathan01347@gmail.com>
2023-11-04 15:09:54 -0300

    refactor(OrderModel)change in data received from order

--- src/models/OrderModel.js
+++ src/models/OrderModel.js
@@ -1,6 +1,15 @@
 const { model, Schema } = require("mongoose")
 
 const orderSchema = new Schema({
+  customer: { type: String, required: true },
+  address: { type: String, required: true },
+  number: { type: Number, required: true },
+  district: { type: String, required: true },
+  city: { type: String, required: true },
+  complement: { type: String, required: true },
+  phone: { type: String, required: true },
+  payment: { type: String, required: true },
+  discount: { type: String, required: true },
   order: { type: Array, required: true },
   total: { type: Number, required: true },
   date: { type: String, required: true },
