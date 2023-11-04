25ca4c7c11a9eb01974e3977c53654fab8f28065
Jonathan Santos <jonathan01347@gmail.com>
2023-11-04 15:12:56 -0300

    feat(RouterOrder)create a new route.

--- src/routes/order/RouteOrder.js
+++ src/routes/order/RouteOrder.js
@@ -4,6 +4,7 @@ const { Router } = require('express');
 const RouterOrder = Router();
 
 RouterOrder.get('/orders', orderController.getAllOrders);
+RouterOrder.get('/order/:id', orderController.getOrderById);
 RouterOrder.post('/create-order', orderController.createOrder);
 
 module.exports = RouterOrder;
