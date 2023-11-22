4965fc50fc1b40c1781b1e830484fd2f4df0c573
Jonathan Santos <jonathan01347@gmail.com>
2023-11-21 22:10:04 -0300

    test(MenuModel)Added tests for the Model layer

---
+++ src/tests/unit/models/MenuModel.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai')
+const { describe, it } = require('mocha')
+const MenuModel = require('../../../models/MenuModel')
+
+
+describe('Create MenuModel with correct schema', () => {
+
+  it('should create a new MenuModel', () => {
+    const menu = new MenuModel({
+      name: 'Menu 1',
+      price: 10,
+      category: 'Frango'
+    })
+    expect(menu.name).to.be.equal('Menu 1')
+    expect(menu.price).to.be.equal(10)
+    expect(menu.category).to.be.equal('Frango')
+  })
+
+})
+
