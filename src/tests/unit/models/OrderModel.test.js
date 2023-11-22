e8d4319f5f502a07fd76ad995d1a17b0039a0892
Jonathan Santos <jonathan01347@gmail.com>
2023-11-21 22:10:22 -0300

    test(OrderModel)Added tests for the Model layer

---
+++ src/tests/unit/models/OrderModel.test.js
@@ -0,0 +1,48 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+const OrderModel = require('../../../models/OrderModel');
+
+describe('Create OrderModel with correct schema', () => {
+
+  it('should create a new OrderModel', () => {
+    const order = new OrderModel({
+      customer: "Jonathan",
+      address: "Rua dos bobos",
+      number: 50,
+      district: "Centro",
+      city: "São Paulo",
+      complement: "Apto 50",
+      phone: "11999999999",
+      payment: "PIX",
+      discount: "Sim",
+      order: [
+        {
+          name: "Menu 1",
+          price: 10,
+          category: "Frango",
+        }
+      ],
+      total: 10,
+      date: "01-01-2021",
+      hours: "12:00:00",
+    });
+
+    expect(order.customer).to.equal("Jonathan");
+    expect(order.address).to.equal("Rua dos bobos");
+    expect(order.number).to.equal(50);
+    expect(order.district).to.equal("Centro");
+    expect(order.city).to.equal("São Paulo");
+    expect(order.complement).to.equal("Apto 50");
+    expect(order.phone).to.equal("11999999999");
+    expect(order.payment).to.equal("PIX");
+    expect(order.discount).to.equal("Sim");
+    expect(order.order[0].name).to.equal("Menu 1");
+    expect(order.order[0].price).to.equal(10);
+    expect(order.order[0].category).to.equal("Frango");
+    expect(order.total).to.equal(10);
+    expect(order.date).to.equal("01-01-2021");
+    expect(order.hours).to.equal("12:00:00");
+    
+  })
+
+});
